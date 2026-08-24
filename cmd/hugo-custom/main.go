420c78c2fe0f01ee1f8d8d7c1e3aa23e969b4b44
BritoAlv <gomzalezbritoalvaro@gmail.com>
2026-08-24 07:13:21 -0400

    hello world in go

---
+++ cmd/hugo-custom/main.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/BritoAlv/hugo_custom/internal/utils"
+)
+
+func main() {
+	fmt.Println("Hello World")
+	utils.Greet("Alvaro")
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/BritoAlv/hugo_custom
+
+go 1.27.0
